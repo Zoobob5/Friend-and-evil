69be44e9aaa16bbf38b59f090ecd1cc5b41fa499
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2022-08-04 20:16:53 -0700

    first

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,21 @@
 // Your code here
-
+class Dragon{
+  constructor(name, color){
+    this.name=name,
+    this.color=color
+  }
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+  static getDragons(...args){
+    // let dragons=[]
+    // for(let i=0; i<args.length; i++){
+    //   dragons.push(args[i].name)
+    // }
+    // return dragons
+    return args.map(i=>i.name)
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
